9bb1ef7aee2550fbed0b8e61490d1b3dc5bc3372
CoderX <85693093+PlanetEditorX@users.noreply.github.com>
2025-11-22 16:51:53 +0800

    Create add_dialer-proxy.js

---
+++ scripts/add_dialer-proxy.js
@@ -0,0 +1,19 @@
+/**
+ * 给所有节点添加 dialer-proxy 字段
+ * 用法：Sub-Store 添加脚本，自动处理订阅内容
+ */
+
+const inArg = $arguments;
+
+function operator(proxies) {
+  proxies.forEach(proxy => {
+    // 给所有节点加上 dialer-proxy
+    proxy['dialer-proxy'] = '🥇 优选链式';
+
+    // 如果只想给特定节点加，可以用名字过滤
+    // if (/VPS|落地/.test(proxy.name)) {
+    //   proxy['dialer-proxy'] = '机场节点';
+    // }
+  });
+  return proxies;
+}
