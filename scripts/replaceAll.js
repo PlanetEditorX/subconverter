09a40fae461f9e291ba0e47a706fe5e391e48b3d
CoderX <85693093+PlanetEditorX@users.noreply.github.com>
2026-07-12 14:27:55 +0800

    Update replaceAll.js

--- scripts/replaceAll.js
+++ scripts/replaceAll.js
@@ -1,5 +1,5 @@
 // @name 全局字符串替换
-// @description 通过参数动态替换订阅内容中的字符串
+// @description 全局替换节点中的字符串
 // @argument oldString 替换前字符串
 // @argument newString 替换后字符串
 
@@ -7,8 +7,8 @@ const oldString = $arguments.oldString || "";
 const newString = $arguments.newString || "";
 
 if (!oldString) {
-    console.log("未设置 oldString 参数");
-    return;
+    console.log("oldString 参数为空");
+    return proxies;
 }
 
 function replaceAll(obj) {
@@ -20,17 +20,14 @@ function replaceAll(obj) {
         return obj.map(item => replaceAll(item));
     }
 
-    if (obj !== null && typeof obj === "object") {
-        const result = {};
-
+    if (obj && typeof obj === "object") {
         for (const key in obj) {
-            result[key] = replaceAll(obj[key]);
+            obj[key] = replaceAll(obj[key]);
         }
-
-        return result;
     }
 
     return obj;
 }
 
-$content = replaceAll($content);
+
+return replaceAll(proxies);
